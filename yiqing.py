2212cf9f3e860a0917f52f47a87516a67b5222fc
cctpp <78080608+cctpp@users.noreply.github.com>
2021-01-28 18:10:44 +0800

    Update yiqing.py

--- yiqing.py
+++ yiqing.py
@@ -76,9 +76,10 @@ def report(usr,pas):
         log.append([[usr,pas],strftime("%Y-%m-%d %H:%M:%S",localtime(his[0]['scrq']/1000))+' '+eval(r.text)["msgText"]+' '+his[0]['xm']])
     else:
         log.append([[usr,pas],strftime("%Y-%m-%d %H:%M:%S",localtime(his[0]['scrq']/1000))+' 已上报  '+his[0]['xm']])
-        browser = launcher.launch()
-        wx = browser.newPage()
-        wx.goto('https://sc.ftqq.com/' + wxts + '.send?text=平安行动打卡成功！'+ his[0]['xm'])
+        async def checkin():
+            browser = await launcher.launch()
+            wx = await browser.newPage()
+            await wx.goto('https://sc.ftqq.com/' + wxts + '.send?text=平安行动打卡成功！'+ his[0]['xm'])
             
 
     sess.close()

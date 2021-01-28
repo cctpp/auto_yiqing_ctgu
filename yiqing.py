b47ca20d2d81278aa427a6b2d38bd9590ade27c0
cctpp <78080608+cctpp@users.noreply.github.com>
2021-01-28 16:43:52 +0800

    Update yiqing.py

--- yiqing.py
+++ yiqing.py
@@ -81,7 +81,7 @@ def report(usr,pas):
                 "text" : title,
                 "desp" : content
             }
-            post(api, data = data)
+            req = requests.post(api, data = data)
             
 
     sess.close()
